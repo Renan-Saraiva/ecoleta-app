1f1018e55e970ed5ade972225f6949835892a211
Renan Magalhães Saraiva <renan.m.saraiva@gmail.com>
2020-06-06 13:40:20 -0300

    inclusão de novas views

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Feather as  Icon } from '@expo/vector-icons'
+import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+
+const Detail = () => {
+    return (
+        <View>
+        </View>
+    )
+}
+
+
+export default Detail
